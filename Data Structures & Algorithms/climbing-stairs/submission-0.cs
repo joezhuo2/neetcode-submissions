8f2151a13f7285b461bdfed1c1409dd88e8e70de
Joe <52431024+joezhuo2@users.noreply.github.com>
2026-08-30 10:36:38 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int ClimbStairs(int n) {     
+        if (n <= 2) return n;
+
+        int prev2 = 1;
+        int prev1 = 2;
+        for (int i = 3; i <= n; i++) {
+            int current = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
